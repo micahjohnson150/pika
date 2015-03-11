3b1db22cba1c6b64e8f8302e0d7e0619a74e8bec
Micah Johnson <micah.johnson150@gmail.com>
2015-03-11 17:55:19 -0600

    running code. Wont converge but all is together.

--- include/kernels/PhaseBoussinesq.h
+++ include/kernels/PhaseBoussinesq.h
@@ -1,82 +1,46 @@
-/**********************************************************************************/
-/*                  Pika: Phase field snow micro-structure model                  */
-/*                                                                                */
-/*                     (C) 2014 Battelle Energy Alliance, LLC                     */
-/*                              ALL RIGHTS RESERVED                               */
-/*                                                                                */
-/*                   Prepared by Battelle Energy Alliance, LLC                    */
-/*                      Under Contract No. DE-AC07-05ID14517                      */
-/*                      With the U. S. Department of Energy                       */
-/**********************************************************************************/
-
 #ifndef PHASEBOUSSINESQ_H
 #define PHASEBOUSSINESQ_H
 
-// MOOSE includes
-#include "Boussinesq.h"
+//Moose Includes
+#include "Kernel.h"
 
-// Pika includes
+//Pika Includes
 #include "PropertyUserObjectInterface.h"
-#include "CoefficientKernelInterface.h"
 
-//Forward Declarations
+// Forward Declarations
 class PhaseBoussinesq;
 
 template<>
 InputParameters validParams<PhaseBoussinesq>();
 
 /**
- * A coefficient diffusion Kernel
- *
- * This Kernel allows to a coefficient to be applied to the diffusion term, that
- * coefficient may be either a scalar value or a scalar material property.
- *
- * This Kernel includes the ability to scale and offset the coefficient. The
- * coefficient (material or scalar) is applied as:
- *     (scale * coefficient + offset) * div(coefficient \nabla u)
- *
- * Also, include the ability to toggle the additional temporal scaling parameter (\xi)
- * as defined by Kaempfer and Plapp (2009). This temporal scalling is applied in
- * additions to the coefficient scaling:
- *     xi * (scale * coefficient + offset) * div(coefficient \nabla u)
+ * Computes a one sided, phase dependent buoyancy based
+ * forcing term for the Navier Stokes Equations in PikaMomentum
  */
-class PhaseBoussinesq :
-  public Boussinesq,
-  public PropertyUserObjectInterface
+class PhaseBoussinesq : 
+    public Kernel,
+    public PropertyUserObjectInterface
 {
 public:
-
-  /**
-   * Class constructor
-   */
   PhaseBoussinesq(const std::string & name, InputParameters parameters);
 
+  virtual ~PhaseBoussinesq(){}
+
 protected:
-
-  /**
-   * Compute residual
-   * Utilizes Boussinesq::computeQpResidual with phase dependency added
-
-   */
   virtual Real computeQpResidual();
-
-  /**
-   * Compute Jacobian
-   * Utilizes Boussinesq::computeQpJacobian with phase dependency added
-   */
   virtual Real computeQpJacobian();
-
- /**
-   * Compute off diagonal jacobian
-   * Utilizes Boussinesq::computeQpOffDiagJacobian  with phase dependency added
-   */
-
-
   virtual Real computeQpOffDiagJacobian(unsigned jvar);
 
+  // Coupled variables
+  VariableValue& _T;
   VariableValue& _phase;
+  unsigned _T_var_number;
   unsigned _phase_var_number;
 
+  Real _T_ref;
+  Real _alpha;
+  Real _rho;
+  RealVectorValue _gravity;
+  unsigned _component;
 };
-
-#endif //MATDIFFUSION_H
+#endif // PHASEBOUSSINESQ_H
