ef99bb137b28d56ae1382da6a2f3cfca13bad03f
Micah Johnson <micah.johnson150@gmail.com>
2015-04-07 12:05:29 -0600

    working lid driven cavity case with ice wallsgit stgit stgit stgit stgit st

--- src/kernels/PhaseNoSlipForcing.C
+++ src/kernels/PhaseNoSlipForcing.C
@@ -11,9 +11,6 @@ InputParameters validParams<PhaseNoSlipForcing>()
 
   //Coefficients
   params.addParam<Real>("h",2.757,"This value helps enforce the no slip condition, caution: instability can occur if too large.");
-//  params.addParam<Real>("coefficient",1e5,"This value enforces the no slip condition by being very large, caution: instability can occur if too large.");
-//  params.addParam<Real>("rho",1,"fluid density");
-  params.addParam<Real>("mu",1,"dyanamic viscosity");
 
   return params;
 }
@@ -33,27 +30,27 @@ PhaseNoSlipForcing::PhaseNoSlipForcing(const std::string & name, InputParameters
   _h(getParam<Real>("h")),
 //  _rho(getParam<Real>("rho"))
   _w_2(getMaterialProperty<Real>("interface_thickness_squared")),
-  _mu(getParam<Real>("h"))
+  _mu(_property_uo.getParam<Real>("dry_air_viscosity"))
 
 {
 }
 
 Real PhaseNoSlipForcing::computeQpResidual()
 {
-  return  _mu * 0.25 * (1.0 - (_phase[_qp] *  _phase[_qp])) * _h * _u[_qp] / _w_2[_qp];
+  //return _mu * 0.25 * (1.0 - (_phase[_qp] * _phase[_qp])) * _h * _u[_qp] * _test[_i][_qp] / _w_2[_qp];
+  return _mu * 0.25 * std::pow(1.0 + _phase[_qp],2.0) * (1.0 -_phase[_qp]) * _h * _u[_qp] * _test[_i][_qp] / _w_2[_qp];
 }
 
 Real PhaseNoSlipForcing::computeQpJacobian()
 {
 
- //return -0.5 * (1.0-_phase[_qp])* _rho * _alpha * _phi[_j][_qp] * _test[_i][_qp] * _gravity(_component);
-  return _mu * 0.25 * (1.0 - _phase[_qp] *  _phase[_qp]) * _h * _phi[_j][_qp] / _w_2[_qp];
+  return 0.0; //-_mu * 0.25 * (1.0 - _phase[_qp] *  _phase[_qp]) * _h * _phi[_j][_qp] / _w_2[_qp];
 }
 
 Real PhaseNoSlipForcing::computeQpOffDiagJacobian(unsigned jvar)
 {
   if(jvar == _phase_var_number)
-    return - _mu * 0.5 *  _phase[_qp] * _h * _u[_qp] / _w_2[_qp];
+    return 0.0;//- _mu * 0.5 *  _phase[_qp] * _h * _u[_qp] / _w_2[_qp];
   else
     return 0.0; 
 }
