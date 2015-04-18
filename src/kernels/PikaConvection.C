9cb1d9b5dc83d0cc950d5fa23c30c9ab187c98f9
Micah Johnson <micah.johnson150@gmail.com>
2015-04-17 18:44:59 -0600

    working on boussinesq flow within  an ice confined box

---
+++ src/kernels/PikaConvection.C
@@ -0,0 +1,95 @@
+/**********************************************************************************/
+/*                  Pika: Phase field snow micro-structure model                  */
+/*                                                                                */
+/*                     (C) 2014 Battelle Energy Alliance, LLC                     */
+/*                              ALL RIGHTS RESERVED                               */
+/*                                                                                */
+/*                   Prepared by Battelle Energy Alliance, LLC                    */
+/*                      Under Contract No. DE-AC07-05ID14517                      */
+/*                      With the U. S. Department of Energy                       */
+/**********************************************************************************/
+
+#include "PikaConvection.h"
+
+template<>
+InputParameters validParams<PikaConvection>()
+{
+  InputParameters params = validParams<Kernel>();
+  params += validParams<PropertyUserObjectInterface>();
+  params += validParams<CoefficientKernelInterface>();
+
+
+  params.addRequiredCoupledVar("phase","variable containing the phase");
+  params.addRequiredCoupledVar("vel_x", "x-velocity");
+  params.addCoupledVar("vel_y", "y-velocity"); // only required in 2D and 3D
+  params.addCoupledVar("vel_w", "z-velocity"); // only required in 3D
+
+  return params;
+}
+
+PikaConvection::PikaConvection(const std::string & name, InputParameters parameters) :
+    Kernel(name, parameters),
+    PropertyUserObjectInterface(name,parameters),
+    CoefficientKernelInterface(name,parameters),
+    // Coupled variables
+    _u_vel(coupledValue("vel_x")),
+    _v_vel(_mesh.dimension() >= 2 ? coupledValue("vel_y") : _zero),
+    _w_vel(_mesh.dimension() == 3 ? coupledValue("vel_w") : _zero),
+
+    // Variable numberings
+    _u_vel_var_number(coupled("vel_x")),
+    _v_vel_var_number(_mesh.dimension() >= 2 ? coupled("vel_y") : libMesh::invalid_uint),
+    _w_vel_var_number(_mesh.dimension() == 3 ? coupled("vel_z") : libMesh::invalid_uint),
+
+    _phase(coupledValue("phase"))
+{
+ // The getMaterialProperty method cannot be replicated in interface
+  if (useMaterial())
+    setMaterialPropertyPointer(&getMaterialProperty<Real>(getParam<std::string>("property")));
+}
+
+Real
+PikaConvection::computeQpResidual()
+{
+   return  0.5 * coefficient(_qp) * (
+                                _u_vel[_qp]*_grad_u[_qp](0) - _u_vel[_qp] * _phase[_qp] * _grad_u[_qp](0) +
+                                _v_vel[_qp]*_grad_u[_qp](1) - _v_vel[_qp] * _phase[_qp] * _grad_u[_qp](1) +
+                                _w_vel[_qp]*_grad_u[_qp](2) - _w_vel[_qp] * _phase[_qp] * _grad_u[_qp](2)) * _test[_i][_qp];
+}
+
+Real
+PikaConvection::computeQpJacobian()
+{
+  RealVectorValue U(_u_vel[_qp], _v_vel[_qp], _w_vel[_qp]);
+ 
+ //Jacobian of convective part
+  
+  return ( 0.5 *coefficient(1.0-_phase[_qp]) *
+       (U*_grad_phi[_j][_qp] + (_phi[_j][_qp]*_grad_u[_qp](_component)))) * _test[_i][_qp];
+}
+Real
+PikaConvection::computeQpOffDiagJacobian(unsigned jvar)
+{
+  // The off Diag Jacobian for u_vel 
+  if (jvar == _u_vel_var_number)
+  {
+    return 0.5 * coefficient(_qp) * (1.0-_phase[_qp]) * _phi[_j][_qp] * _grad_u[_qp](0) * _test[_i][_qp];
+  }
+
+  // The off Diag Jacobian for v_vel 
+  else if (jvar == _v_vel_var_number)
+  {
+
+    return 0.5 * coefficient(_qp) * (1.0-_phase[_qp]) * _phi[_j][_qp] * _grad_u[_qp](1) * _test[_i][_qp];
+
+  }
+
+  // The off Diag Jacobian for w_vel 
+  else if (jvar == _w_vel_var_number)
+  {
+    return 0.5 * coefficient(_qp) * (1.0-_phase[_qp]) * _phi[_j][_qp] * _grad_u[_qp](2) * _test[_i][_qp];
+  }
+
+  else 
+    return 0.0;
+}
